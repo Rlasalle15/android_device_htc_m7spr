47dad6c44f3c2bc2e9e3ed70e15df71f3aab7ecd
Ryan La Salle <ryanlasalle@gmail.com>
2013-12-11 22:52:11 -0500

    Inherit omni vendor common.mk file and change produt name for omni

--- full_m7spr.mk
+++ full_m7spr.mk
@@ -25,11 +25,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit from m7spr device
 $(call inherit-product, device/htc/m7spr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_m7spr
+PRODUCT_NAME := omni_m7spr
 PRODUCT_DEVICE := m7spr
 PRODUCT_BRAND := htc
 PRODUCT_MANUFACTURER := HTC
